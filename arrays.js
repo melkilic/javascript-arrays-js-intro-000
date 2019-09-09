78b675ff26a54f20e232f876519876515c759d40
Melike KILIC <melike180400@gmail.com>
2019-09-09 02:24:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,10 +13,9 @@ function destructivelyAddElementToEndOfArray(chocolateBars,foo){
 chocolateBars.push("foo");
 return chocolateBars;
 }
-// function accessElementInArray(chocolateBars,2){
-//   var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"] ;
-//  return chocolateBars[2];
-// }
+function accessElementInArray(chocolateBars,2){
+  return chocolateBars[2];
+}
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
   chocolateBars.shift()
   return chocolateBars;
@@ -25,7 +24,8 @@ function removeElementFromBeginningOfArray(chocolateBars){
   return chocolateBars.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray (chocolateBars){
-  return chocolateBars.pop();
+  chocolateBars.pop()
+return chocolateBars;
 }
 function removeElementFromEndOfArray (chocolateBars){
 return chocolateBars.slice(0, chocolateBars.length - 1);
