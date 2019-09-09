a7add5d2d86bae3e0e47c4764429a974f7ac3db8
Melike KILIC <melike180400@gmail.com>
2019-09-09 02:04:38 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"] ;
+function addElementToBeginningOfArray(chocolateBars,foo){
+return ["foo",...chocolateBars];
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars,foo){
+  chocolateBars.unshift("foo");
+  return chocolateBars;
+}
+ function addElementToEndOfArray (chocolateBars,foo){
+  return[...chocolateBars,"foo"];
+ }
+function destructivelyAddElementToEndOfArray(chocolateBars,foo){
+chocolateBars.push("foo");
+}
+function accessElementInArray(chocolateBars,2){
+  var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"] ;
+ return chocolateBars[2];
+}
